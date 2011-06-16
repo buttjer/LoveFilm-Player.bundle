7ef0164253a4cc100454de921a2c96a283cee08f
Bird Ian <IanDBird@Gmail.com>
2011-06-16 17:20:29 +0100

    initial plugin

--- Contents/Code/__init__.py
+++ Contents/Code/__init__.py
@@ -1,326 +1,132 @@
-####################################################################################################
+VIDEO_PREFIX = "/video/lovefilm-player"
 
-VIDEO_PREFIX = "/video/blinkbox"
-
-NAME = L('Title')
+NAME = "LoveFilm Player"
 
 ART = 'art-default.jpg'
 ICON = 'icon-default.png'
+ICON_SEARCH = 'icon-search.png'
 
-MOVIES_FREE = 'http://www.blinkbox.com/Movies/Free'
-TV_FREE = 'http://www.blinkbox.com/TV/Free'
+ACTION_URL = 'http://www.lovefilm.com/browse/film/watch-online/subscription/action/?rows=50'
+CHILDREN_URL = 'http://www.lovefilm.com/browse/film/watch-online/subscription/childrens/?rows=50'
+COMEDY_URL = 'http://www.lovefilm.com/browse/film/watch-online/subscription/comedy/?rows=50'
+DRAMA_URL = 'http://www.lovefilm.com/browse/film/watch-online/subscription/drama/?rows=50'
+HORROR_URL = 'http://www.lovefilm.com/browse/film/watch-online/subscription/horror/?rows=50'
+THRILLER_URL = 'http://www.lovefilm.com/browse/film/watch-online/subscription/thriller/?rows=50'
+TV_URL = 'http://www.lovefilm.com/browse/film/watch-online/subscription/television/?rows=50'
+
+SEARCH_URL = 'http://www.lovefilm.com/browse/film/watch-online/subscription/?query=%s'
 
 ####################################################################################################
-
-# This function is initially called by the PMS framework to initialize the plugin. This includes
-# setting up the Plugin static instance along with the displayed artwork.
 def Start():
     
     # Initialize the plugin
-    Plugin.AddPrefixHandler(VIDEO_PREFIX, MainMenu, L('Title'), ICON, ART)
-    Plugin.AddViewGroup("Basic", viewMode = "InfoList", mediaType = "items")
-    Plugin.AddViewGroup("Basic", viewMode = "List", mediaType = "items")
+    Plugin.AddPrefixHandler(VIDEO_PREFIX, MainMenu, NAME, ICON, ART)
+    Plugin.AddViewGroup("InfoList", viewMode = "InfoList", mediaType = "items")
     
     # Setup the artwork associated with the plugin
-    MediaContainer.art = R(ART)
     MediaContainer.title1 = NAME
-    DirectoryItem.thumb = R(ICON)
+    MediaContainer.art = R(ART)
+    MediaContainer.content = 'Items'
 
-# This main function will setup the displayed items. This will depend if the user is currently
-# logged in.
+####################################################################################################
+
 def MainMenu():
-    dir = MediaContainer(disabledViewModes=["Coverflow"], title1 = L('Title'))
-               
-    # Movies
-    dir.Append(Function(
-        DirectoryItem(
-            MovieMenu,
-            L('Movies'),
-            thumb = R(ICON))))
+    dir = MediaContainer(viewGroup='InfoList')
     
-    # TV Shows
-    dir.Append(Function(
-        DirectoryItem(
-            TVMenu,
-            L('TVShows'),
-            thumb = R(ICON))))
+    dir.Append(Function(DirectoryItem(ListItems, "Action", thumb = R(ICON)), url = ACTION_URL))
+    dir.Append(Function(DirectoryItem(ListItems, "Children", thumb = R(ICON)), url = CHILDREN_URL))
+    dir.Append(Function(DirectoryItem(ListItems, "Comedy", thumb = R(ICON)), url = COMEDY_URL))
+    dir.Append(Function(DirectoryItem(ListItems, "Drama", thumb = R(ICON)), url = DRAMA_URL))
+    dir.Append(Function(DirectoryItem(ListItems, "Horror", thumb = R(ICON)), url = HORROR_URL))
+    dir.Append(Function(DirectoryItem(ListItems, "Thriller", thumb = R(ICON)), url = THRILLER_URL))
+    dir.Append(Function(DirectoryItem(ListItems, "TV", thumb = R(ICON)), url = TV_URL))
+    dir.Append(Function(InputDirectoryItem(SearchMenu, "Search", "Please enter the name of the title to search...", thumb=R(ICON_SEARCH))))
+    
+    # Preferences
+    dir.Append(PrefsItem(L('Preferences'), thumb=R('icon-prefs.png')))
     
     return dir
 
 ####################################################################################################
-# Movies
+
+def SearchMenu(sender, query=None):
+    return ListItems(sender, SEARCH_URL % String.Quote(query))
+
 ####################################################################################################
 
-# This is the main function for displaying all available free movies. 
-def MovieMenu(sender, current_page = 0):
-    dir = MediaContainer(disabledViewModes=["Coverflow"], title1=L('Movies'))
+def ListItems(sender, url, replaceParent = False):
+    dir = MediaContainer(viewGroup = 'InfoList', title2 = sender.itemTitle, thumb = R(ICON), replaceParent = replaceParent)
+    
+    # Iterate over the film items located within the page...
+    page = HTML.ElementFromURL(url)
+    for pageItem in page.xpath("//div[@id='film_listings']//div[@class='film_listing sd open fl_detail ']"):
+    
+        # Determine the URL and Title from the page.
+        url = pageItem.xpath(".//h2/a")[0].get('href')
+        title = pageItem.xpath(".//h2/a/text()")[0]
 
-    if current_page != 0:
-        dir.replaceParent = True
+        # [Optional]
+        summary = None
+        try: summary = pageItem.xpath(".//div[@class='read_more']/text()")[0]
+        except: pass
+
+        # [Optional]
+        release_decade = None
+        try: release_decade = pageItem.xpath(".//h2//span/text()")[0]
+        except: pass
+
+        # [Optional]
+        rating = None
+        try: rating = float(pageItem.xpath(".//li[@property='v:rating']")[0].get('content')) * 2
+        except: pass
+
+        # [Optional]
+        cover_art = None
+        try: cover_art = pageItem.xpath(".//img")[0].get('src')
+        except: pass
+        
+        # [Optional]
+        directors = None
+        try: directors = [item.strip() for item in pageItem.xpath(".//div[contains(text(), 'Director: ')]/a/text()") ]
+        except: pass
+        
+        summary_format = "%s \n\n%s%s"
+        if directors != None:
+            summary = summary_format % (summary, "Director(s): ", ", ".join(directors))
+        
+        # [Optional]
+        staring = None
+        try: staring = [item.strip() for item in pageItem.xpath(".//div[contains(text(), 'Starring: ')]/a/text()") ]
+        except: pass
+
+        if staring != None:
+            summary = summary_format % (summary, "Staring: ", ", ".join(staring))
     
-    # Request the appropriate page
-    free_movies_url = MOVIES_FREE + '?page=' + str(current_page + 1)
-    movies = HTTP.Request(free_movies_url)
-    movies_free = HTML.ElementFromString(movies)
-    
-    movie_assets = movies_free.xpath("//div[@class='movieAsset']")
-    for movie_asset in movie_assets:
-		
-        # Parse the movie details to determine all associated content.
-        title_details = parseMovieTitle(movie_asset)
+        # [Optional]
+        certificate = None
+        try: certificate = pageItem.xpath(".//div[@class='certif']/img")[0].get('alt')
+        except: pass
+                
+        if certificate != None:
+            summary = summary_format % (summary, "Certificate: ", certificate)
         
         dir.Append(WebVideoItem(
-            title_details['url'],
-            title = title_details['name'],
-            subtitle = title_details['subtitle'],
-            summary = title_details['description'],
-            thumb = title_details['image']))
+            url.replace('http://','https://'),
+            title = title,
+            subtitle = release_decade,
+            summary = summary,
+            userRating = rating,
+            thumb = cover_art)) 
     
-    next_page = movies_free.xpath("//a[@class='pag_forw bundle']")
-    if len(next_page) > 0:
-        dir.Append(Function(
-            DirectoryItem(
-                MovieMenu,
-                "Next"),
-            current_page = current_page + 1))
+    # If we can find the list item associated with the "Next" url, then we should display an additional option to allow
+    # the user navigate further. The next results will replace the current DirectoryItem.
+    next = page.xpath("//li/a[contains(text(), 'Next >')]")
+    if len(next) > 0:
+        next_url = next[0].get('href')
+        dir.Append(Function(DirectoryItem(ListItems, "Next...", thumb = R(ICON)), url = next_url, replaceParent = True))
     
-    return dir
-
-# This function will parse a specific 'movieAsset' to determine the associated information about
-# the title.
-def parseMovieTitle(item):
-    summary_node = item.xpath(".//div[@class='summary']/a")[0]
-    
-    # Attempt to locate the title's name.
-    name = ''
-    try:
-        name = summary_node.xpath(".//span/text()")[0]
-        name = name.lstrip().rstrip()
-    except:
-        pass
-    
-    # Attempt to locate the url to the title.
-    url = ''
-    try:
-        url = summary_node.get('href')
-    except:
-        pass
-    
-    # Attempt to locate the associated image.
-    image = ''
-    try:
-        image = item.xpath(".//img")[0].get('src')
-    except:
-        pass
-	
-    subtitle = ''
-    try:
-        subtitle = item.xpath(".//div[@class='message']/p/span/text()")[0]
-    except:
-        pass
-    
-    description = ''
-    try:
-        description = item.xpath(".//span[@class='msg']/text()")[0]
-    except:
-        pass
-    
-    parsed = {}
-    parsed['name'] = name
-    parsed['url'] = url
-    parsed['image'] = image
-    parsed['subtitle'] = subtitle
-    parsed['description'] = description
-    
-    return parsed
-
-####################################################################################################
-# TV Shows
-####################################################################################################
-
-# This function displays the top level menu for all availabe TV shows. When selected, it will then
-# display the available series.
-def TVMenu(sender, current_page = 0):
-    dir = MediaContainer(disabledViewModes = ["Coverflow"], title1 = L('TVShows'))
-
-    if current_page != 0:
-        dir.replaceParent = True
-    
-    # Request the appropriate page
-    free_tv_shows_url = TV_FREE + '?page=' + str(current_page + 1)
-    tv_shows = HTTP.Request(free_tv_shows_url)
-    tv_shows_free = HTML.ElementFromString(tv_shows)
-    
-    movie_assets = tv_shows_free.xpath("//div[@class='movieAsset']")
-    for movie_asset in movie_assets:
-		
-        title_details = parseTvTitle(movie_asset)
-        
-        dir.Append(Function(
-            DirectoryItem(
-                TVSeasonMenu,
-                title_details['name'],
-                subtitle = title_details['subtitle'],
-                summary = title_details['description'],
-                thumb = title_details['image']),
-            title_details = title_details))
-
-    next_page = tv_shows_free.xpath("//a[@class='pag_forw bundle']")
-    if len(next_page) > 0:
-        dir.Append(Function(
-            DirectoryItem(
-                TVMenu,
-                "Next"),
-            current_page = current_page + 1))
-    
-    return dir
-
-# This function will parse a specific 'movieAsset' to determine the associated information about
-# the title.
-def parseTvTitle(item):
-    summary_node = item.xpath(".//div[@class='summary']/a")[0]
-    
-    # Attempt to locate the title's name.
-    name = ''
-    try:
-        name = summary_node.xpath(".//span/text()")[0]
-    except:
-        pass
-    
-    # Attempt to locate the url to the title.
-    url = ''
-    try:
-        url = summary_node.get('href')
-    except:
-        pass
-    
-    # Attempt to locate the associated image.
-    image = ''
-    try:
-        image = item.xpath(".//img")[0].get('src')
-    except:
-        pass
-	
-    subtitle = ''
-    try:
-        subtitle = item.xpath(".//div[@class='message']/p/text()")[0]
-    except:
-        pass
-    
-    description = ''
-    try:
-        description = item.xpath(".//span[@class='msg']/text()")[0]
-    except:
-        pass
-    
-    parsed = {}
-    parsed['name'] = name
-    parsed['url'] = url
-    parsed['image'] = image
-    parsed['subtitle'] = subtitle
-    parsed['description'] = description
-    
-    return parsed
-
-# This function displays the available seasons for a specified tv show.
-def TVSeasonMenu(sender, title_details):
-    
-    url = title_details['url']
-
-    tv_seasons_string = HTTP.Request(url)
-    tv_seasons = HTML.ElementFromString(tv_seasons_string)
-
-    # If there is only one series which is available, we should simply return the different episodes immediately
-    tv_season_collection = tv_seasons.xpath("id('selectSeries')/a")
-    if len(tv_season_collection) == 1:
-        return TVEpisodeMenu(sender, title_details['name'], url = url)
-
-    dir = MediaContainer(disabledViewModes = ["Coverflow"], title1 = sender.title1, title2 = title_details['name'])
-            
-    for season in tv_season_collection:
-
-        # We shouldn't show the option to display all series.
-        if season.get('class') == "showAll":
-            continue
-        
-        season_number = season.xpath(".//text()")[0]
-        season_url = season.get('href')
-        
-        dir.Append(Function(DirectoryItem(
-            TVEpisodeMenu,
-            "%s %s" % (L("Season"), season_number),
-            subtitle = title_details['subtitle'],
-            summary = title_details['description'],
-            thumb = title_details['image']),
-            name = title_details['name'],
-            url = season_url))
+    # Check to see if we've found anything
+    if len(dir) == 0:
+        return MessageContainer("Error", "No titles were found!")
 
     return dir
-
-# This function displays the individual episodes available for a specified tv show.
-def TVEpisodeMenu(sender, name = '', url = ''):
-    dir = MediaContainer(disabledViewModes = ["Coverflow"], title1 = sender.title1, title2 = name)
-
-    tv_series_string = HTTP.Request(url)
-    tv_series = HTML.ElementFromString(tv_series_string)
-
-    assets = tv_series.xpath("//div[@class='assetDetailed c']")
-    for asset in assets:
-		
-        title_details = parseTvEpisodeTitle(asset)
-        
-        dir.Append(WebVideoItem(
-            title_details['url'],
-            title = title_details['name'],
-            subtitle = title_details['subtitle'],
-            summary = title_details['description'],
-            thumb = title_details['image']))
-    
-    return dir
-
-# This function will parse a specific 'assetDetailed c' to determine the associated information about
-# the title.
-def parseTvEpisodeTitle(item):
-    
-    # Attempt to locate the title's name.
-    name = ''
-    try:
-        name = item.xpath(".//p[@class='title']/a/text()")[0]
-    except:
-        pass
-    
-    # Attempt to locate the url to the title.
-    url = ''
-    try:
-        node = item.xpath(".//p[@class='title']/a")[0]
-        url = node.get('href')
-    except:
-        pass
-    
-    # Attempt to locate the associated image.
-    image = ''
-    try:
-        image = item.xpath(".//img")[0].get('src')
-    except:
-        pass
-	
-    subtitle = ''
-    try:
-        subtitle = item.xpath(".//p[@class='header']/text()")[0]
-    except:
-        pass
-    
-    description = ''
-    try:
-        description = item.xpath(".//p[@class='info']/text()")[0]
-    except:
-        pass
-    
-    parsed = {}
-    parsed['name'] = name
-    parsed['url'] = url
-    parsed['image'] = image
-    parsed['subtitle'] = subtitle
-    parsed['description'] = description
-    
-    return parsed
